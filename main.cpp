6f729be4f87d692f7c6f1385fa9a5afed9494621
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-12-07 10:19:08 +0200

    финал

--- main.cpp
+++ main.cpp
@@ -1,104 +1,69 @@
 /*
-В подзадаче 1 не более 2000 слов.
-В подзадачах 2 и 3 считать, что размер текста не более 10 кБ.
-
-2. 	Дана строка, содержащая английский текст.
- Если в тексте есть слова-палиндромы длиной более 2-х букв, то вывести слова текста в   соответствии с долей гласных,
-  в противном случае удалить из слов   текста гласные буквы, продублировать согласные и вывести полученные слова в алфавитном порядке.
-
 3. 	В текстовом файле input.txt записан английский текст.
  Найти в тексте слова, содержащие более семи различных букв, выделить их и указать после каждого такого слова в скобках найденные буквы. 
  Полученный текст записать в файл output.txt. Весь текст, кроме найденных слов, должен остаться неизменным, включая и знаки препинания.
 */
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <set>
-#include <cstring>
-#include <algorithm>
+#include <cctype>
 using namespace std;
 
-bool isPalindrome(const char *word) 
-{
-    int len = strlen(word);
-    for (int i = 0; i < len / 2; i++)
-        if (tolower(word[i]) != tolower(word[len - i - 1]))
-            return false;
-    return true;
+bool hasMoreThanSevenUniqueLetters(const string &word, set<char> &uniqueLetters) {
+    uniqueLetters.clear();
+    for (char ch : word) {
+        if (isalpha(ch)) { // Проверяем, является ли символ буквой
+            uniqueLetters.insert(tolower(ch));
+        }
+    }
+    return uniqueLetters.size() > 7;
 }
 
-double vowelRatio(const char *word) 
-{
-    int vowelCount = 0, length = strlen(word);
-    for (int i = 0; i < length; i++)
-        if (strchr("aeiouAEIOU", word[i]))
-            vowelCount++;
-    return static_cast<double>(vowelCount) / length;
-}
+int main() {
+    ifstream inputFile("input.txt");
+    ofstream outputFile("output.txt");
 
-void transformWord(const char *word, char *result) 
-{
-    int j = 0;
-    for (int i = 0; word[i]; i++) 
-    {
-        if (!strchr("aeiouAEIOU", word[i])) 
-        {
-            result[j++] = word[i];
-            result[j++] = word[i]; // Дублируем согласные
-        }
-    }
-    result[j] = '\0';
-}
-
-int main() 
-{
-    char input[2000], transformed[50];
-    cout << "Введите строку: ";
-    cin.getline(input, sizeof(input));
-
-    char *tokens[200]; 
-    int tokenCount = 0;
-
-    char *token = strtok(input, " ");
-    while (token) 
-    {
-        tokens[tokenCount++] = token;
-        token = strtok(nullptr, " ");
+    if (!inputFile.is_open()) {
+        cerr << "Не удалось открыть файл input.txt" << endl;
+        return 1;
     }
 
-    set<string> palindromes;
-    bool hasPalindrome = false;
+    string line;
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        string word;
+        set<char> uniqueLetters;
+        
+        while (ss >> word) {
+            string originalWord = word; // Сохраняем оригинальное слово
 
-    for (int i = 0; i < tokenCount; i++) 
-    {
-        if (strlen(tokens[i]) > 2 && isPalindrome(tokens[i])) 
-        {
-            palindromes.insert(tokens[i]);
-            hasPalindrome = true;
+            // Убираем знаки препинания с конца слова
+            while (!word.empty() && ispunct(word.back())) {
+                word.pop_back();
+            }
+            
+            if (hasMoreThanSevenUniqueLetters(word, uniqueLetters)) {
+                // Формируем строку для вставки
+                string uniqueLettersStr(uniqueLetters.begin(), uniqueLetters.end());
+                word += " (" + uniqueLettersStr + ")";
+            }
+
+            // Восстанавливаем оригинальное слово (с знаками)
+            ss.str("");
+            ss << originalWord; 
+            // Выводим слово (с добавлением уникальных букв, если найдено)
+            outputFile << word << " ";
+            ss.clear();
         }
+        outputFile << endl; // Переход на новую строку
     }
 
-    if (hasPalindrome) 
-    {
-        for (int i = 0; i < tokenCount; i++) 
-        {
-            cout << tokens[i] << " (доля гласных: " << vowelRatio(tokens[i]) << ")\n";
-        }
-    } 
-    else 
-    {
-        set<string> transformedWords;
-
-        for (int i = 0; i < tokenCount; i++) 
-        {
-            transformWord(tokens[i], transformed);
-            transformedWords.insert(transformed);
-        }
-
-        for (const auto &w : transformedWords) 
-        {
-            cout << w << endl;
-        }
-    }
-
+    inputFile.close();
+    outputFile.close();
+    
     return 0;
 }
+
