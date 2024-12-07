1ee3e9a45988a3132640c0dd089d79bd2d114068
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2024-12-07 09:08:17 +0200

    1

--- main.cpp
+++ main.cpp
@@ -15,3 +15,49 @@
  Найти в тексте слова, содержащие более семи различных букв, выделить их и указать после каждого такого слова в скобках найденные буквы. 
  Полученный текст записать в файл output.txt. Весь текст, кроме найденных слов, должен остаться неизменным, включая и знаки препинания.
 */
+#include <iostream>
+#include <set>
+#include <string>
+#include <cctype>
+#include <sstream> 
+using namespace std;
+
+int main() 
+{
+    set<string> wordsSet;
+    char Letter;
+    int N;
+
+    cout << "Введите текст, для окончания ввода введите END:\n";
+    string text, line;
+    while (getline(cin, line) && line != "END") 
+    {
+        text += line + " ";
+    }
+
+    cout << "Введите букву: ";
+    cin >> Letter;
+    cout << "Введите количество слов (N <= 2000): ";
+    cin >> N;
+
+    // Разделение текста на слова
+    istringstream stream(text);
+    string word;
+    while (stream >> word) 
+    {
+        if (!word.empty() && tolower(word[0]) == tolower(Letter)) 
+        {
+            wordsSet.insert(word);
+        }
+    }
+
+    // Результат
+    int count = 0;
+    for (const auto& w : wordsSet) 
+    {
+        cout << w << "\n";
+        if (++count == N) break;
+    }
+
+    return 0;
+}
